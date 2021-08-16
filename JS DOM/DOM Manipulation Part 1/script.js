b0da5c03fb8a2818e7e6e22876868b207aea92c0
Adi Kusuma <86455282+adiksmj@users.noreply.github.com>
2021-08-16 22:15:09 +0800

    Update script.js

--- JS DOM/DOM Manipulation Part 1/script.js
+++ JS DOM/DOM Manipulation Part 1/script.js
@@ -1,5 +1,33 @@
-const judul = document.getElementById('judul');
-judul.innerHTML = 'Gede Adi';
+//element.innerHTML
+// const judul = document.getElementById('judul');
+// judul.innerHTML = 'Gede Adi';
 
-const sectionA = document.querySelector('section#a');
-sectionA.innerHTML = 'hello world';
+// const sectionA = document.querySelector('section#a');
+// sectionA.innerHTML = 'hello world';
+//_____________________________________________
+//element.style.<propertyCSS>
+
+// const judul = document.querySelector('#judul');
+// judul.style.color = 'lightblue';
+// judul.style.backgroundColor = 'salmon';
+
+//_____________________________________________
+//element.setAttribute() ---> will replace the existing class attribute (important!)
+// const judul = document.getElementsByTagName('h1')[0];
+
+// judul.setAttribute('name', 'gede adi');
+
+//____________________________________________
+//element.classlist :
+// element.classList.add() -- adding new class 
+// element.classList.remove() -- removing class
+// element.classList.toggle() -- if already exist it will replace, if not, it will add (true/false)
+// element.classList.item() -- to check class list
+// element.classList.contains() -- to check class list by asking (true/false)
+// element.classList.replace() -- replace class
+
+
+
+
+
+
