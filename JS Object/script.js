8ba5249f346c7ed1cae28538a9e3afe86722a61e
Adi Kusuma <86455282+adiksmj@users.noreply.github.com>
2021-08-22 14:12:24 +0800

    update Prototype Method

--- JS Object/script.js
+++ JS Object/script.js
@@ -62,24 +62,42 @@
 // let adi = new Mahasiswa('Adi Kusuma', 10); //dont forget the keyword new
 
 //4. Object.create()
-const methodMahasiswa = {
-    makan: function (porsi) {
-        this.energi += porsi;
-        console.log(`Halo ${this.nama}, selamat makan !`);
-    },
-    main: function (jam) {
-        this.energi -= jam;
-        console.log(`Halo ${this.nama}, selamat bermain`);
-    }
-}
-function Mahasiswa(nama, energi) {
-    let mahasiswa = Object.create(methodMahasiswa);
-        mahasiswa.nama = nama;
-        mahasiswa.energi = energi;
+// const methodMahasiswa = {
+//     makan: function (porsi) {
+//         this.energi += porsi;
+//         console.log(`Halo ${this.nama}, selamat makan !`);
+//     },
+//     main: function (jam) {
+//         this.energi -= jam;
+//         console.log(`Halo ${this.nama}, selamat bermain`);
+//     }
+// }
+// function Mahasiswa(nama, energi) {
+//     let mahasiswa = Object.create(methodMahasiswa);
+//         mahasiswa.nama = nama;
+//         mahasiswa.energi = energi;
     
     
-        return mahasiswa;
-    }
+//         return mahasiswa;
+//     }
 
-let adi = Mahasiswa('Adi Kusuma', 10);
-let kusuma = Mahasiswa('Kusuma Jaya', 10);
+// let adi = Mahasiswa('Adi Kusuma', 10);
+// let kusuma = Mahasiswa('Kusuma Jaya', 10);
+
+//5. Prototype
+function Mahasiswa(nama, energi) {
+        this.nama = nama,
+        this.energi = energi
+}
+
+Mahasiswa.prototype.makan = function (porsi) {
+    this.energi += porsi;
+    return `Halo ${this.nama}, selamat makan !`;
+};
+Mahasiswa.prototype.main = function (jam) {
+    this.energi -= jam;
+    return `Halo ${this.nama}, selamat bermain`;
+};
+
+let adi = new Mahasiswa('Adi Kusuma', 10);
+let kusuma = new Mahasiswa('Kusuma Jaya', 10);
