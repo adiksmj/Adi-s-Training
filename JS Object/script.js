6d448cd2b79194ca2cd66f14fd386c5931f8a242
Adi Kusuma <86455282+adiksmj@users.noreply.github.com>
2021-08-22 13:20:04 +0800

    Create Object syntax

---
+++ JS Object/script.js
@@ -0,0 +1,62 @@
+//1. Object Literal
+// let mahasiswa1 = {
+//     nama: 'Adi Kusuma',
+//     energi: 10,
+//     makan: function(porsi) {
+//         this.energi = this.energi + porsi;
+//         console.log(`Halo $(this.nama), selamat makan !`);
+//     }
+// }
+
+// let mahasiswa2 = {
+//     nama: 'Adi Kusuma 2',
+//     energi: 10,
+//     makan: function(porsi) {
+//         this.energi = this.energi + porsi;
+//         console.log(`Halo $(this.nama), selamat makan !`);
+//     }
+// }
+
+
+//2. Function Declaration
+// function Mahasiswa(nama, energi) {
+//     let mahasiswa = {};
+//     mahasiswa.nama = nama;
+//     mahasiswa.energi = energi;
+
+//     mahasiswa.makan = function (porsi) {
+//         this.energi += porsi;
+//         console.log(`Halo ${this.nama}, selamat makan !`);
+//     }
+//     mahasiswa.main = function (jam) {
+//         this.energi -= jam;
+//         console.log(`Halo ${this.nama}, selamat bermain`);
+//     }
+
+//     return mahasiswa;
+// }
+
+// let adi = Mahasiswa('Adi Kusuma', 10);
+// let kusuma = Mahasiswa('Kusuma Jaya', 10);
+
+
+//3. Constractor Function
+//keyword new
+function Mahasiswa(nama, energi) {
+    //let mahasiswa = {};
+    this.nama = nama;
+    this.energi = energi;
+    
+    this.makan = function (porsi) {
+        this.energi += porsi;
+        console.log(`Halo ${this.nama}, selamat makan !`);
+    }
+    this.main = function (jam) {
+        this.energi -= jam;
+        console.log(`Halo ${this.nama}, selamat bermain`);
+    }
+
+    //return mahasiswa;
+}
+
+let adi = new Mahasiswa('Adi Kusuma', 10);
